7be4b5521755e977fa51c13ba17e59f62bee6224
Rui Barata <ruipbarata@gmail.com>
2021-10-20 23:18:30 +0100

    Project is now ready to development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+seeds_path = Rails.root.join("db/seeds/*/*_seed.rb")
+
+Dir[seeds_path].sort.each { |seed| load seed }
