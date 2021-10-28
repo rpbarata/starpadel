5094d2aef27425a7a39011436dc86af2299f487a
Rui Barata <ruipbarata@gmail.com>
2021-10-28 21:48:18 +0100

    Translate tineMCE to pt-PT

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -222,4 +222,30 @@ Trestle.configure do |config|
   # Enable or disable remember me functionality. Defaults to true.
   #
   # config.auth.remember.enabled = false
+
+  config.tinymce.default.configure do |c|
+    c.skin = "trestle"
+    c.selector = "textarea.tinymce"
+    c.branding = false
+    c.elementpath = false
+    c.menubar = false
+    c.statusbar = false
+    c.plugins = [
+      :lists,
+      :link,
+      :image,
+      :charmap,
+      :table,
+      :code,
+      :hr,
+      :paste,
+    ]
+    c.toolbar = [
+      "styleselect | bold italic underline strikethrough | subscript superscript hr | alignleft aligncenter alignright alignjustify",
+      "bullist numlist | indent outdent | undo redo | link unlink | image charmap table | code",
+    ]
+    c.link_context_toolbar = true
+    c.convert_urls = false
+    c.language = "pt_PT"
+  end
 end
