5b08a6c8b9bab062cd0e3c3dd5f34b5fe7d60348
ruipbarata <ruipbarata@gmail.com>
2021-11-28 19:59:18 +0000

    correct lessons seed

--- db/seeds/d_client_lessons_groups/client_lessons_groups_seed.rb
+++ db/seeds/d_client_lessons_groups/client_lessons_groups_seed.rb
@@ -9,11 +9,17 @@ if ActiveModel::Type::Boolean.new.cast(ENV.fetch("SEED_LESSONS", "false")) && !C
     ClientLessonsGroup.create!(
       comments: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 10),
       client_id: Client.pluck(:id).sample,
-      lessons_type_id:  LessonsType.pluck(:id).sample
+      lessons_type_id:  LessonsType.pluck(:id).sample,
+      time_period: [0, 1].sample
     )
   end
 
   ClientLessonsGroup.find_each do |client_lessons_group|
     client_lessons_group.update(created_at: Faker::Date.between(from: 2.months.ago, to: Time.zone.today))
   end
+end
+
+
+ClientLessonsGroup.find_each do |client_lessons_group|
+  client_lessons_group.update(time_period: [:green_time, :red_time].sample)
 end
