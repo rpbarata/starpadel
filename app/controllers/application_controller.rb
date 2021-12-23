da444a2411bbd7ee6688e9c05a3acb6b905503eb
ruipbarata <ruipbarata@gmail.com>
2021-12-23 02:08:07 +0000

    Correct crash when voucher does not have validity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_sentry_user
+
   def health
     render(json: "OK", status: :ok)
   end
 
   def after_sign_in_path_for(client)
     clients_path
+  end
+
+  def set_sentry_user
+    if current_client.present?
+      Sentry.set_user(username: current_client.name, id: current_client.id)
+    end
   end
 end
