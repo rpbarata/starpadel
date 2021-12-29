6f70be20c24fa7048765384b8528d78097593a93
ruipbarata <ruipbarata@gmail.com>
2021-12-29 23:07:04 +0000

    temp fix project mouting

--- db/migrate/20211104173715_add_become_member_at_to_clients.rb
+++ db/migrate/20211104173715_add_become_member_at_to_clients.rb
@@ -4,13 +4,13 @@ class AddBecomeMemberAtToClients < ActiveRecord::Migration[6.1]
   def change
     add_column(:clients, :become_member_at, :datetime)
 
-    Client.connection.schema_cache.clear!
-    Client.reset_column_information
+    # Client.connection.schema_cache.clear!
+    # Client.reset_column_information
 
-    reversible do |dir|
-      dir.up do
-        Client.members_of_club.update_all(become_member_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
-      end
-    end
+    # reversible do |dir|
+    #   dir.up do
+    #     Client.members_of_club.update_all(become_member_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
+    #   end
+    # end
   end
 end
