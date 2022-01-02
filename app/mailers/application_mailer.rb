72377605cd27b52d16fc1da9ab25094330cd0f3c
ruipbarata <ruipbarata@gmail.com>
2022-01-02 21:44:02 +0000

    User mailjet to sent email in productions/staging

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@starpadel.pt"
+  default from: ENV.fetch("MAILJET_DEFAULT_FROM")
   layout "mailer"
 end
